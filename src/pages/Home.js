7f36505c12a3868a29ae8890c7d18a0a167907b8
Nazrin Amirrullah bin Zaidi <147366697+nazrinamir@users.noreply.github.com>
2024-07-14 19:53:47 +0800

    added tailwind

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='h-screen w-screen'>
+      <div className='bg-slate-800 w-full h-full'>
+
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
